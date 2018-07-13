bd0b86dbf2da8d7a0881fa8f34c5f4e42cc99e43
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-07-13 16:50:16 +0600

    taxonomies added

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 add_action('init', 'film_post_type_init');
-
+add_action('init', 'create_film_taxonomies', 0);
 function theme_enqueue_styles() {
 
 	$parent_style = 'unite-parent-style';
@@ -53,4 +53,128 @@ function film_post_type_init() {
 
 	register_post_type('film', $args);
 }
+
+// create two taxonomies, genres,countries,years,actors for the post type "film"
+function create_film_taxonomies() {
+	// Genre
+	$labels = array(
+		'name' => _x('Genres', 'taxonomy general name', 'unite-child'),
+		'singular_name' => _x('Genre', 'taxonomy singular name', 'unite-child'),
+		'search_items' => __('Search Genres', 'unite-child'),
+		'all_items' => __('All Genres', 'unite-child'),
+		'parent_item' => __('Parent Genre', 'unite-child'),
+		'parent_item_colon' => __('Parent Genre:', 'unite-child'),
+		'edit_item' => __('Edit Genre', 'unite-child'),
+		'update_item' => __('Update Genre', 'unite-child'),
+		'add_new_item' => __('Add New Genre', 'unite-child'),
+		'new_item_name' => __('New Genre Name', 'unite-child'),
+		'menu_name' => __('Genre', 'unite-child'),
+	);
+
+	$args = array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => array('slug' => 'genre'),
+	);
+
+	register_taxonomy('genre', array('film'), $args);
+	// Country
+	$labels = array(
+		'name' => _x('Countries', 'taxonomy general name', 'unite-child'),
+		'singular_name' => _x('Country', 'taxonomy singular name', 'unite-child'),
+		'search_items' => __('Search Countries', 'unite-child'),
+		'popular_items' => __('Popular Countries', 'unite-child'),
+		'all_items' => __('All Countries', 'unite-child'),
+		'parent_item' => null,
+		'parent_item_colon' => null,
+		'edit_item' => __('Edit Country', 'unite-child'),
+		'update_item' => __('Update Country', 'unite-child'),
+		'add_new_item' => __('Add New Country', 'unite-child'),
+		'new_item_name' => __('New Country Name', 'unite-child'),
+		'separate_items_with_commas' => __('Separate Countries with commas', 'unite-child'),
+		'add_or_remove_items' => __('Add or remove Countries', 'unite-child'),
+		'choose_from_most_used' => __('Choose from the most used Countries', 'unite-child'),
+		'not_found' => __('No Countries found.', 'unite-child'),
+		'menu_name' => __('Country', 'unite-child'),
+	);
+
+	$args = array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var' => true,
+		'rewrite' => array('slug' => 'country'),
+	);
+
+	register_taxonomy('country', 'film', $args);
+
+	// Actor
+	$labels = array(
+		'name' => _x('Actors', 'taxonomy general name', 'unite-child'),
+		'singular_name' => _x('Actor', 'taxonomy singular name', 'unite-child'),
+		'search_items' => __('Search Actors', 'unite-child'),
+		'popular_items' => __('Popular Actors', 'unite-child'),
+		'all_items' => __('All Actors', 'unite-child'),
+		'parent_item' => null,
+		'parent_item_colon' => null,
+		'edit_item' => __('Edit Actor', 'unite-child'),
+		'update_item' => __('Update Actor', 'unite-child'),
+		'add_new_item' => __('Add New Actor', 'unite-child'),
+		'new_item_name' => __('New Actor Name', 'unite-child'),
+		'separate_items_with_commas' => __('Separate Actors with commas', 'unite-child'),
+		'add_or_remove_items' => __('Add or remove Actors', 'unite-child'),
+		'choose_from_most_used' => __('Choose from the most used Actors', 'unite-child'),
+		'not_found' => __('No Actors found.', 'unite-child'),
+		'menu_name' => __('Actor', 'unite-child'),
+	);
+
+	$args = array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => false,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var' => true,
+		'rewrite' => array('slug' => 'actor'),
+	);
+
+	register_taxonomy('actor', 'film', $args);
+	// year
+	$labels = array(
+		'name' => _x('Years', 'taxonomy general name', 'unite-child'),
+		'singular_name' => _x('Year', 'taxonomy singular name', 'unite-child'),
+		'search_items' => __('Search Years', 'unite-child'),
+		'popular_items' => __('Popular Years', 'unite-child'),
+		'all_items' => __('All Years', 'unite-child'),
+		'parent_item' => null,
+		'parent_item_colon' => null,
+		'edit_item' => __('Edit Year', 'unite-child'),
+		'update_item' => __('Update Year', 'unite-child'),
+		'add_new_item' => __('Add New Year', 'unite-child'),
+		'new_item_name' => __('New Year Name', 'unite-child'),
+		'separate_items_with_commas' => __('Separate Years with commas', 'unite-child'),
+		'add_or_remove_items' => __('Add or remove Years', 'unite-child'),
+		'choose_from_most_used' => __('Choose from the most used Years', 'unite-child'),
+		'not_found' => __('No Years found.', 'unite-child'),
+		'menu_name' => __('Year', 'unite-child'),
+	);
+
+	$args = array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => false,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var' => true,
+		'rewrite' => array('slug' => 'Year'),
+	);
+
+	register_taxonomy('year', 'film', $args);
+}
+
 ?>
